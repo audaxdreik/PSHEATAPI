305ae5c63211fc0c8c943b4986f1e541a32c7fa0
Austin D <audaxdreik@users.noreply.github.com>
2017-11-02 21:30:57 -0700

    Update Set-CachedCredentials.ps1

--- PSHEATAPI/data/Set-CachedCredentials.ps1
+++ PSHEATAPI/data/Set-CachedCredentials.ps1
@@ -1,4 +1,4 @@
 # do not prefix domain, login should be 'jdoe' and your current network password
 Write-Host -Object "do not prefix domain (i.e. 'jdoe'), login should be and your current network username and password"
 
-Get-Credential -Message 'HomeStreet Bank HEAT Login' | Export-Clixml -Path $PSScriptRoot\cachedCredentials
+Get-Credential -Message 'HEAT Login' | Export-Clixml -Path $PSScriptRoot\cachedCredentials
